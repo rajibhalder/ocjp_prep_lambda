8fc6c1a5fb67aac35af87ddfc571e9a8cb299eb6
rajib <rajib.halder007@gmail.com>
2021-04-11 00:31:25 +0530

    Some changes

---
+++ src/com/ocjp/preparation/GreetingService.java
@@ -0,0 +1,5 @@
+package com.ocjp.preparation;
+
+public interface GreetingService {
+    void sayMessage(String message);
+}
---
+++ src/com/ocjp/preparation/MathOperation.java
@@ -0,0 +1,5 @@
+package com.ocjp.preparation;
+
+public interface MathOperation {
+    int operation(int a, int b);
+}
