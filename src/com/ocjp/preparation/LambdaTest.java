970b674be6179f57024f063e460779010b67d566
rajib <rajib.halder007@gmail.com>
2021-04-11 21:05:56 +0530

    Some changes

--- src/com/ocjp/preparation/LambdaTest.java
+++ src/com/ocjp/preparation/LambdaTest.java
@@ -39,7 +39,7 @@ public class LambdaTest {
         int a = 5;
 
         // lambda expression to define the calculate method
-        Square s = (int x)->x*x;
+        Square s = (int x) -> x * x;
 
         // parameter passed and return type must be
         // same as defined in the prototype
